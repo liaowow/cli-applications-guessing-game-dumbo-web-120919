d92192dd538f56f0855b1489fc8d0a82aef1d3ac
Annie Liao <liaowow@yahoo.com>
2019-11-26 16:20:25 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,4 +3,11 @@ def run_guessing_game
   random_num = rand(6) + 1
   input = gets.chomp
   
+  if input == random_num
+    puts "You guessed the correct number!"
+    elsif input == 'exit'
+      puts "Goodbye!"
+  else
+    puts ""
+  end`
 end
